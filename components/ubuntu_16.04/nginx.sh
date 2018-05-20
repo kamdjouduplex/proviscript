f595cdccef6609cbb6eeec664f6ec2ec13f332a1
terrylinooo <terrylin.developer@gmail.com>
2018-05-21 03:53:14 +0800

    [modify] nginx.sh - Add arguments, see nginx.sh -h

--- components/ubuntu_16.04/nginx.sh
+++ components/ubuntu_16.04/nginx.sh
@@ -1,18 +1,107 @@
 #!/usr/bin/env bash
-#
-# nginx.sh
-#
-
-# Nginx
-package_version=stable
+#>                   +------------+
+#>                   |  nginx.sh  |   
+#>                   +------------+
+#-
+#- SYNOPSIS
+#-
+#-    nginx.sh [-h] [-i] [-v [version]]
+#-
+#- OPTIONS
+#-
+#-    -v ?, --version=?             Which version of Nginx you want to install?
+#-                                  Accept vaule: stable (default), development (if you want to use the latest development version)
+#-    -h, --help                    Print this help.
+#-    -i, --info                    Print script information.
+#-
+#- EXAMPLES
+#-
+#-    $ ./nginx.sh -v development
+#-    $ ./nginx.sh --version=development
+#-    $ ./nginx.sh
+#+
+#+ IMPLEMENTATION:
+#+
+#+    version    1.01
+#+    copyright  https://github.com/Proviscript/
+#+    license    GNU General Public License
+#+    authors    Terry Lin (terrylinooo)
+#+ 
+#+ CHANGELOGS:
+#+
+#+    2018/05/19 terrylinooo First commit.
+#+    2018/05/20 terrylinooo Add arguments, see nginx.sh -h
+#+
+#================================================================
 
 # Display package information, no need to change.
 os_name="Ubuntu"
 os_version="16.04"
 package_name="Nginx"
 
+# Default, you can overwrite this setting by assigning -v or --version option.
+package_version="stable"
+
+# Print script help
+show_script_help() {
+    echo 
+    head -50 ${0} | grep -e "^#[-|>]" | sed -e "s/^#[-|>]*/ /g"
+    echo 
+}
+
+# Print script info
+show_script_information() {
+    echo 
+    head -50 ${0} | grep -e "^#[+|>]" | sed -e "s/^#[+|>]*/ /g"
+    echo 
+}
+
+# Receive arguments in slient mode.
+if [ "$#" -gt 0 ]; then
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            # Which version of MariaDB you want to install?
+            "-v") 
+                package_version="$2"
+                shift 2
+            ;;
+            "--version=*") 
+                package_version="${1#*=}"; 
+                shift 1
+            ;;
+            # Help
+            "-h"|"--help")
+                show_script_help
+                exit 1
+            ;;
+            # Info
+            "-i"|"--information")
+                show_script_information
+                exit 1
+            ;;
+            "-*")
+                echo "Unknown option: $1" >&2
+                exit 1
+            ;;
+            "*")
+                echo "Unknown option: $1" >&2
+                exit 1
+            ;;
+        esac
+    done
+fi
+
 if [ "$(type -t func_component_welcome)" == function ]; then 
     func_component_welcome "nginx" "${package_version}"
+else
+    echo "  _   _           _                       ";
+    echo " | \ | |   __ _  (_)  _ __   __  __       ";
+    echo " |  \| |  / _\` | | | | '_ \  \ \/ /      ";
+    echo " | |\  | | (_| | | | | | | |  >  <        ";
+    echo " |_| \_|  \__, | |_| |_| |_| /_/\_\       ";
+    echo "          |___/                           ";
+    echo "                                          "
+    echo " Automatic installation by Proviscript.   ";
 fi
 
 echo
